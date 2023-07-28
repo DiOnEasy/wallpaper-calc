607a89a9f7f9f857c647e36406cc4d5511404b5b
DiOnEasy <d11062002d@gmail.com>
2023-07-28 22:14:08 +0300

    added script for mapping material types

---
+++ main.js
@@ -0,0 +1,28 @@
+const width = document.getElementById('width');
+const height = document.getElementById('height');
+const materialType = document.getElementById('material__type');
+const calcButton = document.getElementById('calc__button');
+const square = document.getElementById('square');
+const totalPrice = document.getElementById('total__price')
+
+const materialTypes = [
+    {
+        name: 'Текстура под иней',
+        price: 150
+    },
+    {
+        name: 'Текстура под древний папирус',
+        price: 1500
+    },
+    {
+        name: 'Текстура живопись маслом',
+        price: 354
+    },
+]
+
+for (i = 0; i < materialTypes.length; i++) {
+    materialType.innerHTML += 
+    `
+    <option value="${materialTypes[i].price}">${materialTypes[i].name}</option>
+    `
+};
